f678016fbead5a4d9bc969f9b07b45d39dd1af6f
TOUNGA01 <franck.tounga@5thfloor.be>
2022-12-25 12:57:08 +0100

    add network peering and route table config

--- networks.tf
+++ networks.tf
@@ -53,3 +53,66 @@ resource "aws_subnet" "subnet_1_worker" {
   vpc_id     = aws_vpc.vpc_worker.id
   cidr_block = "192.168.1.0/24"
 }
+
+resource "aws_vpc_peering_connection" "peer" {
+  peer_vpc_id = aws_vpc.vpc_worker.id
+  vpc_id      = aws_vpc.vpc_master.id
+  provider    = aws.region-master
+  peer_region = var.region-worker
+}
+
+resource "aws_vpc_peering_connection_accepter" "accept_peering" {
+  provider                  = aws.region-worker
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  auto_accept = true
+}
+
+resource "aws_route_table" "internet_route_master" {
+  vpc_id   = aws_vpc.vpc_master.id
+  provider = aws.region-master
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw_master.id
+  }
+  route {
+    cidr_block                = "192.168.1.0/24"
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  }
+  lifecycle {
+    ignore_changes = all
+  }
+  tags = {
+    Name = "Master-Region-RT"
+  }
+}
+
+resource "aws_main_route_table_association" "set-master-default-rt-assoc" {
+  route_table_id = aws_route_table.internet_route_master.id
+  vpc_id         = aws_vpc.vpc_master.id
+  provider       = aws.region-master
+}
+
+resource "aws_route_table" "internet_route_worker" {
+  vpc_id   = aws_vpc.vpc_worker.id
+  provider = aws.region-worker
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw_worker.id
+  }
+  route {
+    cidr_block                = "10.0.1.0/24"
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  }
+  lifecycle {
+    ignore_changes = all
+  }
+  tags = {
+    Name = "Worker-Region-RT"
+  }
+}
+
+resource "aws_main_route_table_association" "set-worker-default-rt-assoc" {
+  route_table_id = aws_route_table.internet_route_worker.id
+  vpc_id         = aws_vpc.vpc_worker.id
+  provider       = aws.region-worker
+}
