864fe4c073168ff1e35656ffba4ce9e30597506a
TOUNGA01 <franck.tounga@5thfloor.be>
2022-12-27 21:07:18 +0100

    add key pair

--- instances.tf
+++ instances.tf
@@ -7,3 +7,17 @@ data "aws_ssm_parameter" "linuxAmi-worker" {
   name     = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
   provider = aws.region-worker
 }
+
+resource "aws_key_pair" "master-key" {
+  public_key = file("~/.ssh/id_rsa.pub")
+  key_name   = "jenkins"
+  provider   = aws.region-master
+}
+
+resource "aws_key_pair" "worker-key" {
+  public_key = file("~/.ssh/id_rsa.pub")
+  key_name   = "jenkins"
+  provider   = aws.region-worker
+}
+
+
